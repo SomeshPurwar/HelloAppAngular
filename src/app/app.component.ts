ec67336b08c84ecae2b8bc76a0d48c3eba4c7340
SomeshPurwar <somesh.purwar20@gmail.com>
2025-03-21 22:27:28 +0530

    [Somesh] add : added UC4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  userName: string = "";
   message: string = "Hello from BridgeLabz";
   logoUrl: string = "assets/logo.jpg";
 
